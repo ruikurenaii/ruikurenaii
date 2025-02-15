8e2d1c1a2a89108898638e6b443624611acd52d6
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-02-15 21:54:46 +0800

    Update Mathematics

---
+++ Mathematics/powers.js
@@ -0,0 +1,8 @@
+Math.power = function (a, b) {
+  // Simple mathematical function to raise a number to the nth power
+  return a ** b;
+}
+
+// Usage
+console.log(Math.power(2,7)); // This should return the value 128
+console.log(Math.power(9,4)); // This should return the value 6,561
